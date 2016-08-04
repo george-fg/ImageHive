b1b15822ad33a45d290019e28a6186c775018b48
George Green <email": null>
2016-08-04 14:51:44 +1200

    Styling in progress

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../knexfile' ).development
+var knex = require('knex')(config)
+var tables = require('../tables')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ImageHive!' });
+  res.render('login', { title: 'ImageHive!' });
 });
 
+router.get('/postform', function(req, res, next) {
+  res.render('post_form', { title: 'ImageHive!' });
+});
+
+router.get('/userpost', function(req, res, next) {
+  res.render('user_post', { title: 'ImageHive!', id: 3, users_username: "Frank", imgUrl: "http://i.imgur.com/5a1slAa.png", caption: "Hey! its a train!" });
+});
+
+
+//  res.render('user_post', { title: 'ImageHive!', id: 3, name: "Frank", img_url: "http://i.imgur.com/5a1slAa.png", caption: "Hey! its a train!" });
 
 
 module.exports = router;
